f8c29812b741719c7609302bdbcf885d6a269238
MrMelon54 <github@mrmelon54.com>
2024-02-10 16:51:27 +0000

    Stop adding colytra:ElytraUpgrade to everything

--- common/src/main/java/com/mrmelon54/ArmoredElytra/items/TheIllusiveC4ColytraItem.java
+++ common/src/main/java/com/mrmelon54/ArmoredElytra/items/TheIllusiveC4ColytraItem.java
@@ -2,14 +2,16 @@ package com.mrmelon54.ArmoredElytra.items;
 
 
 import com.mrmelon54.ArmoredElytra.ChestplateWithElytraItem;
-import com.mrmelon54.ArmoredElytra.InternalArrays;
-import net.minecraft.world.item.Item;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 
 public record TheIllusiveC4ColytraItem(ItemStack stack) implements ChestplateWithElytraItem {
     public ItemStack getElytra() {
-        return ItemStack.of(stack.getOrCreateTagElement("colytra:ElytraUpgrade"));
+        CompoundTag tag = stack.getTag();
+        if (tag == null) return null;
+        if (!tag.contains("colytra:ElytraUpgrade", Tag.TAG_COMPOUND)) return null;
+        return ItemStack.of(tag.getCompound("colytra:ElytraUpgrade"));
     }
 
     public ItemStack getChestplate() {
