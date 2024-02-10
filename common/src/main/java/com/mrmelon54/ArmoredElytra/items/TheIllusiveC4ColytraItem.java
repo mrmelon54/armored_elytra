a9d33dd40a72b8cc4d6909a6c2a3afa44f0a9e7e
MrMelon54 <github@mrmelon54.com>
2024-02-10 17:02:40 +0000

    Bump to v1.0.5 - Remove stray colytra:ElytraUpgrade tags

--- common/src/main/java/com/mrmelon54/ArmoredElytra/items/TheIllusiveC4ColytraItem.java
+++ common/src/main/java/com/mrmelon54/ArmoredElytra/items/TheIllusiveC4ColytraItem.java
@@ -7,11 +7,18 @@ import net.minecraft.nbt.Tag;
 import net.minecraft.world.item.ItemStack;
 
 public record TheIllusiveC4ColytraItem(ItemStack stack) implements ChestplateWithElytraItem {
+    private static String COLYTRA_TAG = "colytra:ElytraUpgrade";
+
     public ItemStack getElytra() {
         CompoundTag tag = stack.getTag();
         if (tag == null) return null;
-        if (!tag.contains("colytra:ElytraUpgrade", Tag.TAG_COMPOUND)) return null;
-        return ItemStack.of(tag.getCompound("colytra:ElytraUpgrade"));
+        if (!tag.contains(COLYTRA_TAG, Tag.TAG_COMPOUND)) return null;
+        CompoundTag colytraTag = tag.getCompound(COLYTRA_TAG);
+        if (colytraTag.getAllKeys().isEmpty()) {
+            tag.remove(COLYTRA_TAG);
+            return null;
+        }
+        return ItemStack.of(colytraTag);
     }
 
     public ItemStack getChestplate() {
