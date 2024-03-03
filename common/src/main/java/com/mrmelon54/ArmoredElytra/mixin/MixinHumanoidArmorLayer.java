31407ddbebe8e7930ac05061eada3d5f75bcd295
MrMelon54 <github@mrmelon54.com>
2024-03-03 13:28:44 +0000

    Fixes #16 - Rewrite messy code to improve compat with Show Me Your Skin

--- common/src/main/java/com/mrmelon54/ArmoredElytra/mixin/MixinHumanoidArmorLayer.java
+++ common/src/main/java/com/mrmelon54/ArmoredElytra/mixin/MixinHumanoidArmorLayer.java
@@ -1,7 +1,5 @@
 package com.mrmelon54.ArmoredElytra.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mrmelon54.ArmoredElytra.ArmoredElytra;
 import com.mrmelon54.ArmoredElytra.ChestplateWithElytraItem;
@@ -15,10 +13,10 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = HumanoidArmorLayer.class, priority = 999)
@@ -33,29 +31,28 @@ public abstract class MixinHumanoidArmorLayer<T extends LivingEntity, M extends 
         super(renderLayerParent);
     }
 
+    @Unique
+    private boolean armored_elytra$isFakeArmorPass = false;
+    @Unique
+    private T armored_elytra$currentLivingEntity = null;
+
     @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At("TAIL"))
     private void renderFakeChestplate(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
-        // Using OFFHAND equipment slot to signal that this is the chestplate repeat
-        // This feels less hacky than swapping out the item in the inventory slot
+        // Toggle isFakeArmorPass to signal that this is the chestplate repeat
+        armored_elytra$isFakeArmorPass = true;
+        armored_elytra$currentLivingEntity = livingEntity;
         this.renderArmorPiece(poseStack, multiBufferSource, livingEntity, EquipmentSlot.OFFHAND, i, this.getArmorModel(EquipmentSlot.CHEST));
+        armored_elytra$isFakeArmorPass = false;
     }
 
-    @Redirect(method = "renderArmorPiece", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getItemBySlot(Lnet/minecraft/world/entity/EquipmentSlot;)Lnet/minecraft/world/item/ItemStack;"))
-    private ItemStack renderFakeArmorPiece(LivingEntity instance, EquipmentSlot equipmentSlot) {
-        // only modify if the equipment slot is OFFHAND (the chestplate repeat)
-        if (equipmentSlot != EquipmentSlot.OFFHAND) return instance.getItemBySlot(equipmentSlot);
+    @ModifyVariable(method = "renderArmorPiece", at = @At("STORE"), ordinal = 0)
+    private ItemStack injectedFakeArmorPiece(ItemStack value) {
+        if (!armored_elytra$isFakeArmorPass) return value;
 
         // grab armored elytra from cache, null values are sent as the EMPTY stack to prevent crashes
-        ChestplateWithElytraItem item = ArmoredElytra.armoredElytraMappings.get(instance.getUUID());
+        ChestplateWithElytraItem item = ArmoredElytra.armoredElytraMappings.get(armored_elytra$currentLivingEntity.getUUID());
         if (item == null) return ItemStack.EMPTY;
         ItemStack chestplate = item.getChestplate();
         return chestplate == null ? ItemStack.EMPTY : chestplate;
-    }
-
-    @Inject(method = "renderArmorPiece", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getItemBySlot(Lnet/minecraft/world/entity/EquipmentSlot;)Lnet/minecraft/world/item/ItemStack;", shift = At.Shift.AFTER))
-    private void injectedResetEqupimentSlot(CallbackInfo ci, @Local(argsOnly = true) LocalRef<EquipmentSlot> localRef) {
-        // reset equipment slot value for the rest of the renderArmorPiece method
-        EquipmentSlot equipmentSlot = localRef.get();
-        localRef.set(equipmentSlot == EquipmentSlot.OFFHAND ? EquipmentSlot.CHEST : equipmentSlot);
     }
 }
